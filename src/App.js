6ed609deea00b3942ad01ee390f7cc9c187985ae
Reem <janinareem@gmail.com>
2022-02-08 23:45:30 +0200

    Add the App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Calculator from './component/Calculator';
+import './component/App.css';
+
+function App() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
