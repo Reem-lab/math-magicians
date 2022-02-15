e341db276e3f78842fe2584e194bb3bdaaadd5e3
Reem <janinareem@gmail.com>
2022-02-15 22:25:10 +0200

    Add routes on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Calculator from './component/Calculator';
+import Home from './component/Home';
+import Quotes from './component/quotes';
+import NavBar from './component/NavBar';
 import './component/App.css';
 
 class App extends React.Component {
@@ -9,7 +13,22 @@ class App extends React.Component {
   }
 
   render() {
-    return <Calculator />;
+    return (
+      <>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Calculator">
+            <Calculator />
+          </Route>
+          <Route path="/quotes">
+            <Quotes />
+          </Route>
+        </Switch>
+      </>
+    );
   }
 }
 
