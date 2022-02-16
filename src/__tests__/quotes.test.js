5456eb5d20f600e265a429f949eabb4f55cf28d0
alef-garrido <oa.g11@icloud.com>
2022-02-16 14:23:56 -0600

    Add test for Calculator and quotes components

---
+++ src/__tests__/quotes.test.js
@@ -0,0 +1,9 @@
+import renderer from 'react-test-renderer';
+import Quotes from  '../component/quotes';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Quotes />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
